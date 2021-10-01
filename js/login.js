f7ab4bdcf55c9bfee4afc2d9bca677f374955194
Lat <est.leandro.arevalo@unimilitar.edu.co>
2021-10-01 12:34:06 -0500

    intento de login

--- js/login.js
+++ js/login.js
@@ -1,21 +1,44 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-analytics.js";
 
 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBuehf15Tx-RANkCftP8sAH2uJyeQjjVWw",
+  authDomain: "dinastiaclub-4a8fb.firebaseapp.com",
+  databaseURL: "https://dinastiaclub-4a8fb-default-rtdb.firebaseio.com",
+  projectId: "dinastiaclub-4a8fb",
+  storageBucket: "dinastiaclub-4a8fb.appspot.com",
+  messagingSenderId: "1080239593789",
+  appId: "1:1080239593789:web:b8184a4123d38eb50ccd54",
+  measurementId: "G-468Q4BMWQ9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+import "app/auth"
 function login(){
     var usuario=document.getElementById("usuario_correo").value;
     var clave=document.getElementById("clave").value;
 
-app.auth().signInWithEmailAndPassword(usuario, clave)
-  .then((userCredential) => {
+    app.auth().signInWithEmailAndPassword(email, password)
+   .then((userCredential) => {
     // Signed in
     var user = userCredential.user;
     // ...
-  })
-  .catch((error) => {
+    })
+    .catch((error) => {
     var errorCode = error.code;
     var errorMessage = error.message;
-    window.alert("mal");
-  });}
-
+    });
+}
+export {login};
 
 
 /*function login(){
