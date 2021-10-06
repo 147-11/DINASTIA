7bf1a4ab3fa4d86156807671b49738cd9f139b6b
Lat <est.leandro.arevalo@unimilitar.edu.co>
2021-10-06 17:36:53 -0500

    cambios

--- public/js/login.js
+++ public/js/login.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-analytics.js";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword } from "./firebase/auth";
 
 const auth = getAuth();
 
