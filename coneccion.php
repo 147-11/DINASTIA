bd297020070d1acbe5d51b20bbe9c42fd028bf4a
Lat <est.leandro.arevalo@unimilitar.edu.co>
2021-10-08 18:48:19 -0500

    enlace con base de datos

---
+++ coneccion.php
@@ -0,0 +1,22 @@
+<?php
+$servername = 'localhost';
+$username = 'root';
+$password = 'root';
+$db = 'bddinastia';
+$port = 3306;
+
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    echo "<p> error al acceder al servidor </p>";
+    die("Connection failed: " . $conn->connect_error);
+}
+if (!mysqli_select_db($conn, $db))
+{
+    echo "<p> class='error'>- error al acceder a la base de datos </p>";
+    die("Connection failed: ");
+}
+?>
