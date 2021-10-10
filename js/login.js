194b5db4232e6b3482a772199a9fb2c37639f6fc
Lat <est.leandro.arevalo@unimilitar.edu.co>
2021-10-10 00:13:27 -0500

    intento de login

--- js/login.js
+++ js/login.js
@@ -1,8 +1,53 @@
+const mysql = require('mysql');
+const squel = require('squel');
+
+let conexion = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'bddinastia'
+});
+
+conexion.connect;
+
+let consulta = squel.select()
+    .field('usuario')
+    .field('contraseña')
+
+console.log('Consulta SQL:', consulta.toString());
+
+conexion.query(consulta.toString(), function(error, registros, campos) {
+    if (error) {
+        throw error;
+    }
+
+    registros.forEach(function(registro, indice, arreglo) {
+
+var a=getElementById('acceder');
+      a=function login(){
+        var usuario=document.getElementById("usuario_correo").value;
+        var clave=document.getElementById("clave").value;
+      
+        if(usuario==registro.usuario && clave==registro.clave){
+          window.location.href = "index.html";
+        }
+        else{
+          window.alert("Usuario o contraseña inválidos");
+        }
+      }
+    });
+
+    conexion.end();
+});
+
+
+
+
 function login(){
   var usuario=document.getElementById("usuario_correo").value;
   var clave=document.getElementById("clave").value;
 
-  if(usuario=="DinastíaClubFP" && clave=="*Dinastiaclub21*"){
+  if(usuario=="" && clave==""){
     window.location.href = "index.html";
   }
   else{
--- login.php
+++ login.php
@@ -26,9 +26,29 @@
                     </section>   
                 </div>
                 <div class="acceder">
-                    <input onclick=login() type="submit" value="Acceder">
+                    <input onclick=login() type="submit" value="Acceder" id="acceder">
                 </div>
             </div>
     </main>
+    <script>
+        function login(){
+            <?php
+            $login="SELECT * FROM `login`";
+            $login_r=mysqli_query($conn, $login);
+            while($login_mostrar=mysqli_fetch_assoc($login_r) ){
+        ?>
+            var clave=document.getElementById("clave").value;
+            var usuario=document.getElementById("usuario_correo").value;
+
+            if(usuario=="<?php echo login_mostrar['usuario']?>" && clave=="<?php echo login_mostrar['clave']?>"){
+            window.location.href = "index.html";
+            else{
+                window.alert("Usuario o contraseña inválidos");
+            }
+            <?php> } ?>
+        }
+        
+    }
+    </script>
 </body>
 </html>
