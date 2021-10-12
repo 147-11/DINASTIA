23f9d69689c488089c98654ccf1576d6075bd2d4
Lat <est.leandro.arevalo@unimilitar.edu.co>
2021-10-11 19:14:45 -0500

    login completo

--- login.php
+++ login.php
@@ -23,14 +23,36 @@
                     </section>
                     <section>
                         <img src="img/CONTRASEÑA.png" id="contraseña" alt="contraseña">
-                        <input type="password" id="clave" placeholder="Contraseña">
+                        <input  type="password" id="clave" placeholder="Contraseña">
                     </section>   
                 </div>
                 <div class="acceder">
-                    <input onclick=login() type="submit" value="Acceder">
+                    <input onclick=login() id="log" type="submit" value="Acceder" >
                 </div>
             </div>
-    </main>
-    <script src="js/login.js" ></script>
+</main>
+    <script >
+    <?php
+        $login="SELECT * FROM `login`";
+        $login_r=mysqli_query($conn, $login);
+         while($login_mostrar=mysqli_fetch_assoc($login_r)){
+            ?>
+            var u = '<?php echo $login_mostrar['usuario'] ?>';
+            var c = '<?php echo $login_mostrar['clave'] ?>';
+    <?php } ?>
+
+    function login(){
+    var usuario=document.getElementById("usuario_correo").value;
+    var clave=document.getElementById("clave").value;
+
+  
+    if(usuario==u && clave==c){
+      window.location.href = "index.php";
+    }
+    else{
+      window.alert("Usuario o contraseña inválidos");
+    }
+}
+    </script>
 </body>
 </html>
