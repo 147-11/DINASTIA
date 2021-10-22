edef42a13b26569879ebc39fe051c49de4448aa7
Lat <est.leandro.arevalo@unimilitar.edu.co>
2021-10-21 20:57:47 -0500

    editar productos incompleto

---
+++ js/editorproductos.js
@@ -0,0 +1,18 @@
+var comparar = "dinastia1234";
+if (sessionStorage.getItem("usuario") == comparar){
+    
+    document.getElementById('cerrarsesionclub').style.display = 'block';
+
+    //producto 1
+    document.getElementById('editarp1').style.display = 'block';
+
+    //producto 2
+    document.getElementById('editarp2').style.display = 'block';
+
+    //producto 3
+    document.getElementById('editarp3').style.display = 'block';
+
+    //producto 4
+    document.getElementById('editarp4').style.display = 'block';
+
+}
