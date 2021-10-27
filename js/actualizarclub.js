19c151fb2e1db925121354fcf761ee8844f4932c
Lat <est.leandro.arevalo@unimilitar.edu.co>
2021-10-27 17:40:22 -0500

    club editar texto completo sin imagen
    sin ajuste de css

---
+++ js/actualizarclub.js
@@ -0,0 +1,135 @@
+function mision(){
+    
+    document.getElementById('seguromision').style.display = 'block';
+    document.getElementById('contenidom').style.display = 'none';
+}
+
+function seguromision(a) {
+    var titulomision= $('#tm').val();
+    var descripcionmision =$('#dm').val();
+    if (a == 1){
+        $.post('php/actualizarclub.php',{tm:titulomision, dm:descripcionmision }, function(data, status){
+            console.log(data+" "+status);
+            alert("Información Guardada");
+            window.location.href = "club.html";
+        });
+    } else {
+        alert("Modificación Cancelada");
+        document.getElementById('seguromision').style.display = 'none';
+        document.getElementById('contenidom').style.display = 'flex';
+    }
+}
+
+function vision(){
+    
+    document.getElementById('segurovision').style.display = 'block';
+    document.getElementById('contenidov').style.display = 'none';
+}
+
+function segurovision(a) {
+    var titulovision= $('#tv').val();
+    var descripcionvision =$('#dv').val();
+    if (a ==1){
+        $.post('php/actualizarclub.php',{tv:titulovision, dv:descripcionvision }, function(data, status){
+            console.log(data+" "+status);
+            alert("Información Guardada");
+            window.location.href = "club.html";
+        });
+    } else {
+        alert("Modificación Cancelada");
+        document.getElementById('segurovision').style.display = 'none';
+        document.getElementById('contenidov').style.display = 'flex';
+    }
+}
+
+// OBJETIVO GENERAL
+function OG(){
+    
+    document.getElementById('seguroOG').style.display = 'block';
+    document.getElementById('contenidoog').style.display = 'none';
+}
+
+function seguroOG(a) {
+    var tituloOG= $('#tog').val();
+    var descripcionOG =$('#dog').val();
+    if (a == 1){
+        $.post('php/actualizarclub.php',{tog:tituloOG, dog:descripcionOG }, function(data, status){
+            console.log(data+" "+status);
+            alert("Información Guardada");
+            window.location.href = "club.html";
+        });
+    } else {
+        alert("Modificación Cancelada");
+        document.getElementById('seguroOG').style.display = 'none';
+        document.getElementById('contenidoog').style.display = 'flex';
+    }
+}
+
+// OBJETIVOS ESPECIFICOS
+function goe(){
+    
+    document.getElementById('segurogoe').style.display = 'block';
+    document.getElementById('contenidooe').style.display = 'none';
+}
+
+function segurogoe(a) {
+    var titulogoe= $('#toe').val();
+    var descripciongoe =$('#doe').val();
+    if (a == 1){
+        $.post('php/actualizarclub.php',{toe:titulogoe, doe:descripciongoe }, function(data, status){
+            console.log(data+" "+status);
+            alert("Información Guardada");
+            window.location.href = "club.html";
+        });
+    } else {
+        alert("Modificación Cancelada");
+        document.getElementById('segurogoe').style.display = 'none';
+        document.getElementById('contenidooe').style.display = 'flex';
+    }
+}
+
+// HISTORIA
+function historia(){
+    
+    document.getElementById('segurohistoria').style.display = 'block';
+    document.getElementById('contenidoh').style.display = 'none';
+}
+
+function segurohistoria(a) {
+    var titulohistoria= $('#th').val();
+    var descripcionhistoria =$('#dh').val();
+    if (a == 1){
+        $.post('php/actualizarclub.php',{th:titulohistoria, dh:descripcionhistoria}, function(data, status){
+            console.log(data+" "+status);
+            alert("Información Guardada");
+            window.location.href = "club.html";
+        });
+    } else {
+        alert("Modificación Cancelada");
+        document.getElementById('segurohistoria').style.display = 'none';
+        document.getElementById('contenidoh').style.display = 'flex';
+    }
+}
+
+// POLITICAS DE BIENESTAR
+function pb(){
+    
+    document.getElementById('seguropb').style.display = 'block';
+    document.getElementById('contenidopb').style.display = 'none';
+}
+
+function seguropb(a) {
+    var titulopb= $('#tpb').val();
+    var descripcionpb =$('#dpb').val();
+    if (a == 1){
+        $.post('php/actualizarclub.php',{tpb:titulopb, dpb:descripcionpb }, function(data, status){
+            console.log(data+" "+status);
+            alert("Información Guardada");
+            window.location.href = "club.html";
+        });
+    } else {
+        alert("Modificación Cancelada");
+        document.getElementById('seguropb').style.display = 'none';
+        document.getElementById('contenidopb').style.display = 'flex';
+    }
+}
---
+++ php/actualizarclub.php
@@ -0,0 +1,69 @@
+<?php
+function conn(){
+    return mysqli_connect('localhost','root','root','bddinastia');
+}
+
+$conn=conn();
+
+//mision
+    $titulo_club_mision = $_POST['tm'];
+    $descripcion_club_mision = $_POST['dm'];
+
+    if($titulo_club_mision != null){
+    mysqli_query ($conn, "UPDATE club_page set titulo_club_page ='$titulo_club_mision' WHERE club_page.id_club_page = 1");}
+    if($descripcion_club_mision != null){
+    mysqli_query ($conn, "UPDATE club_page set descripcion_club_page ='$descripcion_club_mision' WHERE club_page.id_club_page = 1");}
+
+
+//vision
+    $titulo_club_vision = $_POST['tv'];
+    $descripcion_club_vision = $_POST['dv'];
+
+    if($titulo_club_vision != null){
+    mysqli_query ($conn, "UPDATE club_page set titulo_club_page ='$titulo_club_vision' WHERE club_page.id_club_page = 2");}
+    if($descripcion_club_vision != null){
+    mysqli_query ($conn, "UPDATE club_page set descripcion_club_page ='$descripcion_club_vision' WHERE club_page.id_club_page = 2");}
+
+//objetivo general
+    $titulo_club_OG = $_POST['tog'];
+    $descripcion_club_OG = $_POST['dog'];
+
+    if($titulo_club_OG != null){
+    mysqli_query ($conn, "UPDATE club_page set titulo_club_page ='$titulo_club_OG' WHERE club_page.id_club_page = 3");}
+    if($descripcion_club_OG != null){
+    mysqli_query ($conn, "UPDATE club_page set descripcion_club_page ='$descripcion_club_OG' WHERE club_page.id_club_page = 3");}
+
+
+//objetivos especificos
+    $titulo_club_goe = $_POST['toe'];
+    $descripcion_club_goe = $_POST['doe'];
+
+    if($titulo_club_goe != null){
+    mysqli_query ($conn, "UPDATE club_page set titulo_club_page ='$titulo_club_goe' WHERE club_page.id_club_page = 4");}
+    if($descripcion_club_goe != null){
+    mysqli_query ($conn, "UPDATE club_page set descripcion_club_page ='$descripcion_club_goe' WHERE club_page.id_club_page = 4");}
+
+
+//HISTORIA
+    $titulo_club_historia = $_POST['th'];
+    $descripcion_club_historia = $_POST['dh'];
+
+    if($titulo_club_historia != null){
+    mysqli_query ($conn, "UPDATE club_page set titulo_club_page ='$titulo_club_historia' WHERE club_page.id_club_page = 5");}
+    if($descripcion_club_historia != null){
+    mysqli_query ($conn, "UPDATE club_page set descripcion_club_page ='$descripcion_club_historia' WHERE club_page.id_club_page = 5");}
+
+
+
+
+//politicas bienestar
+    $titulo_club_pb = $_POST['tpb'];
+    $descripcion_club_pb = $_POST['dpb'];
+
+    if($titulo_club_pb != null){
+    mysqli_query ($conn, "UPDATE club_page set titulo_club_page ='$titulo_club_pb' WHERE club_page.id_club_page = 6");}
+    if($descripcion_club_pb != null){
+    mysqli_query ($conn, "UPDATE club_page set descripcion_club_page ='$descripcion_club_pb' WHERE club_page.id_club_page = 6");}
+
+
+?>
